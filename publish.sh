ccb7da402980fae2db96915a5cad5cb44e14dbad
Gaurav Nyaupane <mail.gauravnyaupane@gmail.com>
2022-08-27 08:22:01 +0545

    CI fix

--- publish.sh
+++ publish.sh
@@ -9,7 +9,7 @@ const commit_message = "Automated: Release V${new_version}";
 
 cp.execSync("mkdir -p destination_repo")
 cp.execSync("cp -r target_repo/.git destination_repo/");
-cp.execSync("cp -r dist/ destination_repo");
+cp.execSync("cp -r build/* destination_repo");
 cp.execSync("cp -r README.md destination_repo/");
 cp.execSync("cp -r LICENSE destination_repo/");
 cp.execSync("cd destination_repo && git add -all && git commit -m '${commit_message}' && git push");
