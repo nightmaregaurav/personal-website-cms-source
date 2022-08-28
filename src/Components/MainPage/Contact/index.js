0430df1b397c91d1acc5ecd9f4c178575f81be52
Gaurav Nyaupane <mail.gauravnyaupane@gmail.com>
2022-08-28 09:04:34 +0545

    Fix: Unexpected whitespace before property then  no-whitespace-before-property

--- src/Components/MainPage/Contact/index.js
+++ src/Components/MainPage/Contact/index.js
@@ -29,7 +29,7 @@ const Contact = () => {
                         method: "POST",
                         body: JSON.stringify(data),
                         headers: {"Content-type": "application/json; charset=UTF-8"}
-                    }) .then(() => {
+                    }).then(() => {
                         submit.innerText = "Sent!"
                         setTimeout(() => {
                             name.value("");
