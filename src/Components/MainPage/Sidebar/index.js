bc07328514c87e387a271d5e910a237a41e3f899
Gaurav Nyaupane <mail.gauravnyaupane@gmail.com>
2022-08-29 13:12:54 +0545

    fix: sidebar not autoactivating

--- src/Components/MainPage/Sidebar/index.js
+++ src/Components/MainPage/Sidebar/index.js
@@ -37,9 +37,7 @@ const Sidebar = () => {
     const config = useConfig();
 
     useEffect(() => {
-        return () => {
-            activateSidebarOnly();
-        };
+        activateSidebarOnly();
     }, []);
 
     return (
