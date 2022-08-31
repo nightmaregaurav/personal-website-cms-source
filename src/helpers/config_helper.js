94893e61b7cdbd2113ac97cbeeec04dcf4dfb025
Gaurav Nyaupane <mail.gauravnyaupane@gmail.com>
2022-08-31 09:13:15 +0545

    Now config only loads after 30 seconds of previous load. If page is reloaded

--- src/helpers/config_helper.js
+++ src/helpers/config_helper.js
@@ -2,19 +2,34 @@ import {useMemo} from "react";
 
 export function useConfigGetter() {
     useMemo(() => {
-        const req = new XMLHttpRequest();
-        req.addEventListener("load", (e)=>{
-            try{
-                localStorage.setItem("config", e.target.responseText);
-            } catch(_){
+        let config_added_on = JSON.parse(localStorage.getItem("config_added_on"));
+        let need_to_load_config;
+        if (config_added_on) {
+            let config_added_on_date = new Date(config_added_on);
+            let current_date = new Date();
+            let diff = current_date - config_added_on_date;
+            let diff_in_seconds = diff / 1000;
+            need_to_load_config = diff_in_seconds > 30;
+        } else {
+            need_to_load_config = true;
+        }
+
+        if (need_to_load_config) {
+            const req = new XMLHttpRequest();
+            req.addEventListener("load", (e) => {
+                try {
+                    localStorage.setItem("config", e.target.responseText);
+                    localStorage.setItem("config_added_on", JSON.stringify(new Date()));
+                } catch (_) {
+                    localStorage.setItem("config", "{}");
+                }
+            });
+            req.addEventListener("error", _ => {
                 localStorage.setItem("config", "{}");
-            }
-        });
-        req.addEventListener("error", _ => {
-            localStorage.setItem("config", "{}");
-        });
-        req.open("GET", "/config.json");
-        req.send();
+            });
+            req.open("GET", "/config.json");
+            req.send();
+        }
     }, []);
 }
 
