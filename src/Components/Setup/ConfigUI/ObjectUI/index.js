05f67c40d18f8276b3a292f543c022af5241703c
Gaurav Nyaupane <mail.gauravnyaupane@gmail.com>
2022-10-13 14:17:43 +0545

    Description Tooltip added

--- src/Components/Setup/ConfigUI/ObjectUI/index.js
+++ src/Components/Setup/ConfigUI/ObjectUI/index.js
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './index.scss'
+import ReactTooltip from "react-tooltip";
 
 const ObjectUI = ({onChange, isGhPage, info, name}) => {
+    const [disabled, setDisabled] = useState(false);
+    const [showTooltip, setShowTooltip] = useState(true);
+    // noinspection JSUnresolvedVariable
+    const cardinality = info.cardinality ?? "Compulsory";
+    const description = info.description ?? "";
+    const cancellable = () => cardinality === "Optional";
+    const disable = () => {
+        setDisabled(true);
+    }
+    const enable = () => {
+        setDisabled(false);
+    }
+
+    function getContent() {
+        return <>
+            <span className={"mx-2"}>objectUI</span>
+            <span className={"btn btn-danger"} onClick={() => onChange("abc~def", 123)}>click me</span>
+        </>
+    }
     return (
         <>
             <div className="container" style={{minWidth: "70vw"}}>
                 <div className="panel panel-primary">
                     <div className="panel-body">
                         <span className="text-on-panel">
-                            <i className={"panel-action bi-x-circle-fill text-danger me-2"}/>
-                            <i className={"panel-action bi-plus-circle-fill text-success me-2"}/>
+                            {cancellable()?<>
+                                {disabled?
+                                    <i className={"panel-action bi-plus-circle-fill text-success me-2"} onClick={enable}/>:
+                                    <i className={"panel-action bi-x-circle-fill text-danger me-2"} onClick={disable}/>
+                                }
+                            </>: null}
+                            {(description !== "")?<>
+                                <i data-tip={description} className={"panel-action bi-question-circle-fill text-warning me-2"} onMouseLeave={() => {
+                                    setShowTooltip(false);
+                                    setTimeout(() => setShowTooltip(true), 50);
+                                }}/>
+                                {showTooltip? <ReactTooltip />: null}
+                            </>: null}
                             <b className={"panel-title"}>{name}</b>
-                            <i className={"panel-action bi-question-circle text-primary ms-2"}/>
                         </span>
                         <div className={"panel-content"}>
-                            <span className={"btn btn-danger"} onClick={() => onChange("abc~def", 123)}>click me</span>
-                            objectUI
+                            {getContent()}
                         </div>
                     </div>
                 </div>
