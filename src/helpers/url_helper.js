1a0a662eb8cb5e2e64ff513c162f37bbb445a312
Gaurav Nyaupane <mail.gauravnyaupane@gmail.com>
2022-10-18 19:20:02 +0545

    Added important hemper that will be needed for ImageUrlUI

--- src/helpers/url_helper.js
+++ src/helpers/url_helper.js
@@ -6,3 +6,13 @@ export function isValidUrl(urlString){
         return false;
     }
 }
+export function isValidImgUrl(url, callback){
+    const img = new Image();
+    try{
+        img.onload = () => callback(true);
+        img.onerror = () => callback(false);
+        img.src = url;
+    } catch (_) {
+        callback(false);
+    }
+}
