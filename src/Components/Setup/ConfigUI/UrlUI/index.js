ad8fba36573b176e3312503c572ae88b4fed2a9a
Gaurav Nyaupane <mail.gauravnyaupane@gmail.com>
2022-10-28 17:41:39 +0545

    URL UI Added

--- src/Components/Setup/ConfigUI/UrlUI/index.js
+++ src/Components/Setup/ConfigUI/UrlUI/index.js
@@ -1,12 +1,117 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import './index.scss'
+import {getValueFromName, useConfigValue} from "../../../../helpers/config_helper";
+import {getLabelFromName} from "../../../../helpers/setup_helper";
+import ReactTooltip from "react-tooltip";
+import {isValidUrl} from "../../../../helpers/url_helper";
 
 const UrlUI = ({onChange, info, name, parent_disabledStatus, removable=false}) => {
-    return (
+    const default_value = getValueFromName(name, "");
+    const [urlUiValue, setUrlUiValue] = useConfigValue("", onChange, name);
+    const [oldValue, setOldValue] = useState(default_value);
+    const [isValid, setIsValid] = useState(true);
+    useEffect(() => {
+        setUrlUiValue(default_value);
+    }, [default_value]);
+
+    const [disabledStatus, setDisabledStatus] = useState(false);
+    const [showTooltip, setShowTooltip] = useState(true);
+
+    // noinspection JSUnresolvedVariable
+    const cardinality = info?.minCardinality ?? "Compulsory";
+    const description = info?.description ?? "";
+    const pattern_validation = info?.validation?.pattern ?? null;
+    const min_length_validation = info?.validation?.minLength ?? null;
+    const max_length_validation = info?.validation?.maxLength ?? null;
+
+    const validate = useCallback(() => {
+        let valid = true;
+        if(cardinality === "Compulsory" && urlUiValue === ""){
+            valid = false;
+        }
+        if(pattern_validation){
+            valid = valid && RegExp(pattern_validation).test(urlUiValue);
+        }
+        if(min_length_validation){
+            valid = valid && urlUiValue.length >= min_length_validation;
+        }
+        if(max_length_validation){
+            valid = valid && urlUiValue.length <= max_length_validation;
+        }
+
+        valid = valid && isValidUrl(urlUiValue);
+        setIsValid(valid);
+    }, [urlUiValue, cardinality, max_length_validation, min_length_validation, pattern_validation]);
+
+    useEffect(() => {
+        validate();
+    }, [urlUiValue, validate]);
+
+    const cancellable = () => cardinality === "Optional";
+    const disable = () => {
+        if(!removable){
+            setOldValue(urlUiValue);
+        }
+        setUrlUiValue(default_value);
+        setDisabledStatus(true);
+    }
+    const enable = () => {
+        setUrlUiValue(oldValue)
+        setDisabledStatus(false);
+    }
+    const isDisabled = () => disabledStatus || parent_disabledStatus;
+    const isRemoved = () => isDisabled() && removable;
+
+    const callSetter = (v) => {
+        setUrlUiValue(v);
+    }
+
+    return (<>{isRemoved() ? null :
         <>
-          urlUI
+            <div className={`url-ui-container ui-${name} container`} style={isDisabled()?{opacity:0.50}:null}>
+                <div className={"input-container"}>
+                    <input className={`url-ui-input`} disabled={isDisabled()} type={"url"} id={name} name={name} autoComplete={"off"} aria-labelledby={`placeholder${name}`} value={urlUiValue} data-value={urlUiValue} data-is-valid={isValid.toString()} onChange={(e) => callSetter(e.target.value)} onBlur={(_) => setUrlUiValue(urlUiValue.trim())} />
+                    <span className={"placeholder-elements"}>
+                        <span className={"placeholder-text"} id={`placeholder${name}`}>{getLabelFromName(name)}</span>
+                        <span className={"placeholder-buttons"}>
+                            {cancellable()?<>
+                                {isDisabled()?
+                                    <i className={"panel-action bi-plus-circle-fill text-success me-2"} onClick={enable}/>:
+                                    <i className={"panel-action bi-x-circle-fill text-danger me-2"} onClick={disable}/>
+                                }
+                            </>: null}
+                            {(description !== "")?<>
+                                <i data-tip="" data-for={name} className={"panel-action bi-question-circle-fill text-warning me-2"} onMouseLeave={() => {
+                                    setShowTooltip(false);
+                                    setTimeout(() => setShowTooltip(true), 50);
+                                }}/>
+                            </>: null}
+                        </span>
+                        <span className={"ms-auto pe-1 ps-1 placeholder-info"}>
+                            {(urlUiValue.length > 0) || (urlUiValue.length === 0 && !isValid) ? <small className={"placeholder-status"}>
+                                {isValid ? <i className={"bi-check-circle-fill text-success me-1"}/> : <i className={"bi-x-circle-fill text-danger me-1"}/>}
+                            </small> : null}
+                            {(urlUiValue.length > 0) && ((min_length_validation !== null) || (max_length_validation !== null)) ? <small className={"placeholder-length"}>
+                                [
+                                {urlUiValue.length}
+                                /
+                                {min_length_validation !== null ? min_length_validation : null}
+                                {((min_length_validation !== null) && (max_length_validation !== null)) ? "-" : null}
+                                {max_length_validation !== null ? max_length_validation : null}
+                                ]
+                            </small> : null}
+                        </span>
+                    </span>
+                </div>
+            </div>
+            {showTooltip? <ReactTooltip id={name}>
+                <b>{getLabelFromName(name)}:</b><br/>{description}
+            </ReactTooltip> : null}
         </>
-    );
+    }</>);
 };
 
 export default UrlUI;
+
+// URL: Simple TextBoxUI with URL regex valdation
+// Take: Hint, Name
