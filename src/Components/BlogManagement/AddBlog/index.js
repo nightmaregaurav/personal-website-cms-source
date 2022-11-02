bd825ab4eeff6ceec27f1eb6edd2c61a721b7ae9
Gaurav Nyaupane <mail.gauravnyaupane@gmail.com>
2022-11-02 14:17:49 +0545

    Added base sketch of blog management system

---
+++ src/Components/BlogManagement/AddBlog/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "../index.scss";
+import SunEditor, {buttonList} from "suneditor-react";
+import 'suneditor/dist/css/suneditor.min.css';
+
+
+const AddBlog = () => {
+    return (
+        <>
+            <div className={"container"}>
+
+                <SunEditor
+                    defaultValue="<p>The editor's default value</p>"
+                    setOptions={{
+                        buttonList: buttonList.complex,
+                    }}
+                    height={"80vh"}
+                />
+            </div>
+        </>
+    );
+};
+
+export default AddBlog;
---
+++ src/Components/BlogManagement/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./index.scss";
+
+const BlogManagement = () => {
+    return (<>
+        listing blogs
+    </>);
+};
+
+export default BlogManagement;
